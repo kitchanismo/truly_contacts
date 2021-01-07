4a7ef322f398465dd34300d45a72ca49a11e607a
kitchan <cristian.betsayda@gmail.com>
2021-01-07 21:12:00 +0800

    add interceptor package

--- src/utils/http.tsx
+++ src/utils/http.tsx
@@ -1,12 +1,15 @@
 import axios, { AxiosError } from 'axios'
 import { apiUrlProd, apiUrlDev } from 'configs/index.json'
+import createAuthRefreshInterceptor, {
+  AxiosAuthRefreshOptions,
+} from 'axios-auth-refresh'
 import jwtDecode from 'jwt-decode'
 import { getDecodeToken } from './helper'
 
-const axiosInstance = axios.create()
+//const axiosInstance = axios.create()
 
 //intercept requests
-axiosInstance.interceptors.request.use((config) => {
+axios.interceptors.request.use((config) => {
   config.baseURL =
     process.env.NODE_ENV === 'development' ? apiUrlDev : apiUrlProd
 
@@ -20,31 +23,62 @@ axiosInstance.interceptors.request.use((config) => {
 })
 
 //intercept errors
-axiosInstance.interceptors.response.use(
+// axios.interceptors.response.use(
+//   (response) => response,
+//   (error: AxiosError) => {
+//     if (error.message === 'Network Error') {
+//       throw Error(error.message)
+//     }
+//     const originalRequest = error.config
+//     //refresh a token if access token expired
+//     if (error.response?.status === 403) {
+//       const refreshToken = localStorage.getItem('refresh-token')
+//       return axios
+//         .post('/auth/refresh-token', { refreshToken })
+//         .then(({ data }) => {
+//           localStorage.setItem('access-token', data.accessToken)
+//           return axios(originalRequest)
+//         })
+//     }
+//     throw error
+//   },
+// )
+
+axios.interceptors.response.use(
   (response) => response,
   (error: AxiosError) => {
     if (error.message === 'Network Error') {
       throw Error(error.message)
     }
-    const originalRequest = error.config
-    //refresh a token if access token expired
-    if (error.response?.status === 403) {
-      const refreshToken = localStorage.getItem('refresh-token')
-      return axiosInstance
-        .post('/auth/refresh-token', { refreshToken })
-        .then(({ data }) => {
-          localStorage.setItem('access-token', data.accessToken)
-          return axiosInstance(originalRequest)
-        })
-    }
+
     throw error
   },
 )
 
+createAuthRefreshInterceptor(
+  axios,
+  (failedRequest) => {
+    console.log('sdsd')
+    const refreshToken = localStorage.getItem('refresh-token')
+    return axios
+      .post('/auth/refresh-token', { refreshToken })
+      .then(({ data }) => {
+        localStorage.setItem('access-token', data.accessToken)
+        failedRequest.response.config.headers['Authorization'] =
+          'Bearer ' + data.accessToken
+        return Promise.resolve()
+      })
+  },
+  {
+    statusCodes: [403],
+    pauseInstanceWhileRefreshing: true,
+  },
+)
+
 export default {
-  get: axiosInstance.get,
-  post: axiosInstance.post,
-  put: axiosInstance.put,
-  delete: axiosInstance.delete,
-  axiosInstance,
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete,
+  axios,
 }
